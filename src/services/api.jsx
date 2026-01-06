634a6b30d3e5feb9724f53e959736612f5fcdce7
Harsha Srinatth Ryali <harhsasrinatth5599@gmail.com>
2026-01-06 19:16:26 +0530

    Update api.jsx second

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL
+  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
   withCredentials: true,           // if you need cookies
 });
 
